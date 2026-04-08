d33416f24e107c19c3e443b968f1264253fce9d6
naomi <g00470124@atu.ie>
2026-04-08 14:12:12 +0100

    Add side menu for navigation; enhance search page layout and styling; implement theme toggle functionality

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Add this
-import { FormsModule } from '@angular/forms'; // Add this
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms'; 
 import { RouterLink } from '@angular/router';
 import { MangaService } from '../services/manga-service';
 import { 
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -12,5 +16,14 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
+    importProvidersFrom(
+      IonicStorageModule.forRoot({
+        name: 'manga_db',
+        driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+      })
+    ), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 });
